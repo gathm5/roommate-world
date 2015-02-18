db2e610e3943ddce5ed1e9bd8b9a79f832b1d144
Gautham Stalin <Gautham.Stalin@monitise.com>
2015-02-18 15:57:47 -0800

    business objects

---
+++ app/scripts/services/business/apartment.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('roommateWorldApp')
+  .factory('business/apartment', function () {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+    // Public API here
+    return {
+      someMethod: function () {
+        return meaningOfLife;
+      }
+    };
+  });
